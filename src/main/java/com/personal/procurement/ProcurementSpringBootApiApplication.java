5157fb54119997ce79309801cb9d59410c597a06
Felizardo Chirindja <felizardo.chirindja@gmail.com>
2026-07-11 12:57:02 +0200

    project setup

---
+++ src/main/java/com/personal/procurement/ProcurementSpringBootApiApplication.java
@@ -0,0 +1,12 @@
+package com.personal.procurement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcurementSpringBootApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ProcurementSpringBootApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/procurement/ProcurementSpringBootApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.procurement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcurementSpringBootApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
