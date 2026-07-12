42486de5414efd6ce03557249a3f8563e1c3d6b1
Felizardo Chirindja <felizardo.chirindja@gmail.com>
2026-07-12 16:03:59 +0200

    refactor: update RequestForQuotation import path in repository
    Update the import statement for RequestForQuotation entity in the repository interface to reflect the correct package structure under business/entities. This change ensures proper entity mapping and resolves potential compilation issues.

--- src/main/java/com/personal/procurement/modules/quotation/repositories/RequestForQuotationRepository.java
+++ src/main/java/com/personal/procurement/modules/quotation/repositories/RequestForQuotationRepository.java
@@ -2,6 +2,6 @@ package com.personal.procurement.modules.quotation.repositories;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import com.personal.procurement.modules.quotation.RequestForQuotation;
+import com.personal.procurement.modules.quotation.business.entities.RequestForQuotation;
 
 public interface RequestForQuotationRepository extends MongoRepository<RequestForQuotation, String> {}
