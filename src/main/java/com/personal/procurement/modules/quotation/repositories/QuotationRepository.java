f9d430581dfc119a396ae8741b3f2bb7d38cdb12
Felizardo Chirindja <felizardo.chirindja@gmail.com>
2026-07-12 16:03:35 +0200

    fix: update Quotation entity import path in repository
    Corrected the import statement for Quotation entity in QuotationRepository to reflect the new package structure where the entity is now located under the business.entities subpackage.

--- src/main/java/com/personal/procurement/modules/quotation/repositories/QuotationRepository.java
+++ src/main/java/com/personal/procurement/modules/quotation/repositories/QuotationRepository.java
@@ -2,6 +2,6 @@ package com.personal.procurement.modules.quotation.repositories;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import com.personal.procurement.modules.quotation.Quotation;
+import com.personal.procurement.modules.quotation.business.entities.Quotation;
 
 public interface QuotationRepository extends MongoRepository<Quotation, String> {}
