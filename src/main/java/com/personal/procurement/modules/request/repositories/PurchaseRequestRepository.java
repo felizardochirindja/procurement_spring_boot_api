d4ebd6196f1fe372a42d1e36bc44bfe3ea977e32
Felizardo Chirindja <felizardo.chirindja@gmail.com>
2026-07-12 16:03:47 +0200

    refactor: update PurchaseRequest import path in repository
    Update the import statement for PurchaseRequest entity in PurchaseRequestRepository
    to reflect the new package structure. The entity has been moved from the request
    module root to the business/entities subpackage for better organization.

--- src/main/java/com/personal/procurement/modules/request/repositories/PurchaseRequestRepository.java
+++ src/main/java/com/personal/procurement/modules/request/repositories/PurchaseRequestRepository.java
@@ -2,6 +2,6 @@ package com.personal.procurement.modules.request.repositories;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import com.personal.procurement.modules.request.PurchaseRequest;
+import com.personal.procurement.modules.request.business.entities.PurchaseRequest;
 
 public interface PurchaseRequestRepository extends MongoRepository<PurchaseRequest, String> {}
